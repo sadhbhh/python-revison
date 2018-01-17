26cbdc287e2d29330e8792f1b0de32f91b32eec6
Sadhbh Eddison <15seddison@stjosephsrush.ie>
2018-01-17 10:32:06 +0000

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,12 +1,12 @@
 from time import sleep
 print("hiya")
 ans = ["banana", "carrot", "bicycle", "frog"]
-del ans[1]
 sleep(1)
 
 while True:
   print("how are you today?")
   rea=input("")
+  sleep(1)
   
   if rea=="good":
     print("give a more interesting answer")
@@ -19,15 +19,15 @@ while True:
     print("good 4 u")
     break
     
-def abc();
-  print ans
+def abc():
+  print(ans)
   print("that's a list")
   print("isn't it pretty")
 
 abc()
 
 num = [1, 2, 3, 4, 5, 6, 7, 8]
-print num
+print()num
 for prime in num:
   print(prime)
 
