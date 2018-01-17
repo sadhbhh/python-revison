704453bde8da60c966a850d892fafe1b823e58ba
Sadhbh Eddison <15seddison@stjosephsrush.ie>
2018-01-17 10:23:17 +0000

    Create stuff.py

---
+++ stuff.py
@@ -0,0 +1,34 @@
+from time import sleep
+print("hiya")
+ans = ["banana", "carrot", "bicycle", "frog"]
+del ans[1]
+sleep(1)
+
+while True:
+  print("how are you today?")
+  rea=input("")
+  
+  if rea=="good":
+    print("give a more interesting answer")
+    
+  elif rea=="giraffe":
+    print("good answer")
+    break
+    
+  else:
+    print("good 4 u")
+    break
+    
+def abc();
+  print ans
+  print("that's a list")
+  print("isn't it pretty")
+
+abc()
+
+num = [1, 2, 3, 4, 5, 6, 7, 8]
+print num
+for prime in num:
+  print(prime)
+
+    
